dc1e6628013256f93662f2ac58742a9d3bae29b8
Victor-0rtiz <vicjosneros@gmail.com>
2025-07-28 13:52:02 -0600

    style: format indentation in DIMENSIONNACIONALIDAD constant

--- src/app/services/data/dimensionnacionalidad.ts
+++ src/app/services/data/dimensionnacionalidad.ts
@@ -1,5 +1,5 @@
 export const DIMENSIONNACIONALIDAD = [
-     { id: '1', name: 'Natural de Nicaragua' },
+    { id: '1', name: 'Natural de Nicaragua' },
     { id: '2', name: 'Nacionalizado' },
     { id: '3', name: 'Extranjero' },
 ]
